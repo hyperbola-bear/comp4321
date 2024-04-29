fe00aaed3210ae62262751b35b41a97df4ed377d
emmannyyy <emmanueler97@gmail.com>
2024-04-30 06:00:07 +0800

    added Ngram and Ngramindex

--- NgramIndex.java
+++ NgramIndex.java
@@ -56,12 +56,46 @@ public class NgramIndex {
         }
     }
 
-    public void deleteEntry(Vector<String> bigrams, int docId) throws IOException {
+    public void addTrigramEntry(String key,int docId) throws IOException {
+        try {
+            HashSet<Integer> docs = (HashSet<Integer>)trigram.get(key);
+            if (docs != null) {
+                docs.add(docId); 
+                trigram.put(key,docs); 
+            } else {
+                docs = new HashSet<Integer>();
+                docs.add(docId);
+                trigram.put(key,docs);
+            }
+        } catch (java.io.IOException ex){ 
+            System.err.println(ex.toString());
+        }
+    }
+
+    public void deleteBigramEntry(Vector<String> bigrams, int docId) throws IOException {
         try {
             HashSet<Integer> docs = null;
             for (int i=0; i<bigrams.size();i++) {
                 String key = bigrams.get(i); 
                 docs = (HashSet<Integer>)bigram.get(key);
+                if (docs != null) {
+                    docs.remove(docId);
+                } else {
+                    continue; 
+                }
+            }
+            
+        } catch (java.io.IOException ex) {
+            System.err.println(ex.toString());
+        }
+    }
+
+    public void deleteTrigramEntry(Vector<String> trigrams, int docId) throws IOException {
+        try {
+            HashSet<Integer> docs = null;
+            for (int i=0; i<trigrams.size();i++) {
+                String key = trigrams.get(i); 
+                docs = (HashSet<Integer>)trigram.get(key);
                 if (docs != null) {
                     docs.remove(docId);
                 } else {
