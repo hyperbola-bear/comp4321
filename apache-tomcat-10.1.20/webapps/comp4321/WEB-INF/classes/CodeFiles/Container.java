3e3965b29354b4e0a2008759ca8b8c14d840b059
hyperbola-bear <hung.hin.wang@gmail.com>
2024-05-05 01:17:03 +0900

    wtf

---
+++ apache-tomcat-10.1.20/webapps/comp4321/WEB-INF/classes/CodeFiles/Container.java
@@ -0,0 +1,24 @@
+package CodeFiles;
+
+import java.util.Vector;
+import java.io.IOException;
+import java.io.Serializable;
+
+public class Container implements Serializable {
+    private static final long serialVersionUID = 1L;
+    public Vector<String> childLinks;
+    public int pageSize;
+    public long lastModificationDate;
+    public Vector<String> title;
+
+    public Container(Vector<String> childLinks, int pageSize, long lastModificationDate, Vector<String> title) {
+        this.childLinks = childLinks;
+        this.pageSize = pageSize;
+        this.lastModificationDate = lastModificationDate;
+        this.title = title;
+    }
+
+    public long getLastModificationDate() {
+        return this.lastModificationDate;
+    }
+}
---
+++ apache-tomcat-10.1.20/webapps/comp4321/index.jsp
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Search Engine</title>
+    <link rel="stylesheet" href="styles.css" />
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="utils/utils.js"></script>
+    <script src="utils/getData.js"></script>
+  </head>
+
+  <body>
+    <main>
+      <jsp:include page="html/inputForm.html" />
+
+      <!-- <jsp:include page="html/searchResults.html" /> -->
+    </main>
+    <footer>
+      <p>COMP4321</p>
+    </footer>
+  </body>
+</html>
